511dac960dca19c81eb25a1d6d0206ee2d6ced5c
jacobh <jacobh@l4mobile.com>
2016-05-04 14:22:03 -0700

    UWP deep linking poc. - put a breakpoint at default.js:12 - debug app in emulator - open emulator browser - change location to mybrand://host/url (or whatever you'd like using the mybrand schema)

---
+++ DeepLinkingExample/js/default.js
@@ -0,0 +1,33 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkId=232509
+(function () {
+	"use strict";
+
+	var app = WinJS.Application;
+	var activation = Windows.ApplicationModel.Activation;
+
+	app.onactivated = function (args) {
+	    if (args.detail.kind === activation.ActivationKind.protocol) {
+            //here's our uri. do whatever we need to at this point
+	        console.log(args.detail.detail[0].uri.absoluteUri)//.__proto__.uri.__proto__.absoluteUri);
+	    }
+        //else...or maybe not
+		if (args.detail.kind === activation.ActivationKind.launch) {
+			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+				// TODO: This application has been newly launched. Initialize your application here.
+			} else {
+				// TODO: This application was suspended and then terminated.
+				// To create a smooth user experience, restore application state here so that it looks like the app never stopped running.
+			}
+			args.setPromise(WinJS.UI.processAll());
+		}
+	};
+
+	app.oncheckpoint = function (args) {
+		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+		// You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+		// If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+	};
+
+	app.start();
+})();
